15721dd1db361815295d9eab67439545ec906bb0
Swapnil Srivastava <142904704+Swapnilden@users.noreply.github.com>
2024-03-19 11:11:17 +0530

    Delete app/editor directory

--- app/editor/script.js
+++
@@ -1,7 +0,0 @@
-function enrichMarkdown() {
-    const markdownInput = document.getElementById('markdown-input').value;
-    const enrichedMarkdown = document.getElementById('enriched-markdown');
-    
-    // For now, let's just display the original Markdown content as enriched Markdown
-    enrichedMarkdown.innerHTML = markdownInput;
-}
